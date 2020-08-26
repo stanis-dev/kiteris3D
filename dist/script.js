a24e0c120b5db703e6bd96f9841e91f75ac44e5a
stanis-dev <stanis.samisco@gmail.com>
2020-08-26 13:33:41 +0200

    Menu toggle and subMeny toggle are implemented in JS

--- dist/script.js
+++ dist/script.js
@@ -1,10 +1,10 @@
-const menuBtn = document.querySelector('.toggler');
-const dropdownMenu = document.querySelector('.toggled');
+const menuBtn = document.querySelector('.menu-expand-btn');
+const dropdownMenu = document.querySelector('.collapse-toggle');
 let menuOpen = false;
 
-const subMenuBtn = document.querySelector('.toggler');
-const dropdownMenu = document.querySelector('.toggled');
-let menuOpen = false;
+const subMenuBtn = document.querySelector('.sub-menu-expand-btn');
+const subDropdownMenu = document.querySelector('.submenu-collapse-toggle');
+let subMenuOpen = false;
 
 menuBtn.addEventListener('click', () => {
   if (!menuOpen) {
@@ -15,5 +15,20 @@ menuBtn.addEventListener('click', () => {
     menuBtn.classList.remove('open');
     dropdownMenu.classList.remove('open');
     menuOpen = false;
+  }
+});
+
+subMenuBtn.addEventListener('click', () => {
+  const parentMenu = document.querySelector('.collapse-toggle.open');
+  if (!subMenuOpen) {
+    subMenuBtn.classList.add('open');
+    subDropdownMenu.classList.add('open');
+    parentMenu.style.height = '470px';
+    subMenuOpen = true;
+  } else {
+    subMenuBtn.classList.remove('open');
+    subDropdownMenu.classList.remove('open');
+    parentMenu.style.height = '370px';
+    subMenuOpen = false;
   }
 });
