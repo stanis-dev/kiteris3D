de320b60e2bbeabe025df37c6a1ff64efc98b573
stanis-dev <stanis.samisco@gmail.com>
2020-08-26 16:07:55 +0200

    Desktop menu finished

--- dist/script.js
+++ dist/script.js
@@ -10,25 +10,30 @@ menuBtn.addEventListener('click', () => {
   if (!menuOpen) {
     menuBtn.classList.add('open');
     dropdownMenu.classList.add('open');
+    dropdownMenu.style.height = '370px';
+
     menuOpen = true;
   } else {
     menuBtn.classList.remove('open');
     dropdownMenu.classList.remove('open');
+    dropdownMenu.style.height = '0';
+
+    if (subDropdownMenu) subDropdownMenu.classList.remove('open');
+
     menuOpen = false;
   }
 });
 
 subMenuBtn.addEventListener('click', () => {
-  const parentMenu = document.querySelector('.collapse-toggle.open');
   if (!subMenuOpen) {
     subMenuBtn.classList.add('open');
     subDropdownMenu.classList.add('open');
-    parentMenu.style.height = '470px';
+    dropdownMenu.style.height = '470px';
     subMenuOpen = true;
   } else {
     subMenuBtn.classList.remove('open');
     subDropdownMenu.classList.remove('open');
-    parentMenu.style.height = '370px';
+    dropdownMenu.style.height = '370px';
     subMenuOpen = false;
   }
 });
